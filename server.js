e80f2577b1e250378036dbeb2051805fcacface0
raj <rraju12@gmail.com>
2018-07-16 23:52:11 +0100

    Add handlebarsto the solutions

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 //3rd party modules
 const express = require('express')
 const formidable = require("express-formidable");
-
+const hbs = require('hbs')
 //variables
 const port = 3000
 const pathPostsFile = __dirname + "/data/posts.json";
@@ -18,7 +18,10 @@ allposts = JSON.parse(fs.readFileSync(pathPostsFile).toString());
 
 //serving public assets middleware
 app.use(express.static('public'))
-
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+hbs.registerHelper("getCurrentYear", () => new Date().getFullYear());
+hbs.registerHelper("setUpper", text => text.toUpperCase());
 app.use(formidable());
 
 // router
@@ -31,6 +34,7 @@ app.post('/create-post', (req, res) => {
     fs.writeFileSync(pathPostsFile, JSON.stringify(allposts));
     res.sendFile(pathPostsFile);
 })
+require('./router/routes')(app) //routes for handlebars
 app.listen(port, (req, res) => {
     console.log("Server is runnig on port:", port); 
 })

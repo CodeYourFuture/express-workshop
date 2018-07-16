74d8213d1137f84aacaef5b62825ea2010888d5b
Francisca Vargas <francisca@franciscavargas.me>
2018-07-16 23:06:36 +0100

    Finished point 8

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
 
-app.get("/student", function(req, res) {
-  console.log("New request to Student page at " + Date());
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<html><body><h1>This is student Page.</h1></body></html>");
-  res.end();
+const formidable = require("express-formidable");
+
+app.use(formidable());
+
+app.post("/subscribe", function(req, res) {
+  var email = req.fields.mail;
+
+  fs.readFile(__dirname + "/data/posts.json", function(error, content) {
+    var mailingList = JSON.parse(content);
+    var date = Date.now();
+
+    mailingList[date] = email;
+    var newContent = JSON.stringify(mailingList);
+
+    fs.writeFile(__dirname + "/data/posts.json", newContent, function(error) {
+      res.end("success!");
+    });
+  });
 });
 
 app.get("*", function(req, res) {

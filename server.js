e67aec09b3911d7bbd4c27e53d7d72d1c4fb70d9
Francisca Vargas <francisca@franciscavargas.me>
2018-07-17 22:52:50 +0100

    Handlebars exercise

--- server.js
+++ server.js
@@ -1,25 +1,53 @@
+const exphbs = require("express-handlebars");
 const fs = require("fs");
 const express = require("express");
 const app = express();
-
-app.use(express.static("public"));
-
 const formidable = require("express-formidable");
 
+app.use(express.static("public"));
 app.use(formidable());
+
+app.set("views", __dirname + "/views");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.post("/subscribe", function(req, res) {
   var email = req.fields.mail;
 
   fs.readFile(__dirname + "/data/posts.json", function(error, content) {
+    //Convert the text in JSON to an object
     var mailingList = JSON.parse(content);
-    var date = Date.now();
 
+    // Adding the content to the object
+    var date = Date.now();
     mailingList[date] = email;
+
+    // Convert the object to string in JSON format
     var newContent = JSON.stringify(mailingList);
 
+    // Write the file with the whole new content
     fs.writeFile(__dirname + "/data/posts.json", newContent, function(error) {
       res.end("success!");
+    });
+  });
+});
+
+app.get("/subscribers", function(req, res) {
+  // Read file
+  fs.readFile(__dirname + "/data/posts.json", function(error, content) {
+    //Convert the text in JSON to an object
+    var mailingList = JSON.parse(content);
+
+    //Convert the object in an array with the emails
+    var emails = Object.keys(mailingList).map(function(key) {
+      return mailingList[key];
+    });
+
+    console.log(emails);
+
+    // array emails to handlebars
+    res.render("subscribers", {
+      subscribersMail: emails
     });
   });
 });

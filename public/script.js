acc08250be9546a3598c3afaeb7269eb833fc7c5
mattwood4 <matthewwood4@gmail.com>
2018-07-22 14:18:53 +0100

    add server with 2 routes, modified the client js to work with the data server, added form to index html

--- public/script.js
+++ public/script.js
@@ -5,74 +5,69 @@ if (document.readyState !== 'loading') {
 }
 
 function ready () {
-    getBlogposts('/get-posts');
-
+    getBlogposts("/posts");
     // send posts to server
     var form = document.querySelector('form');
     form.addEventListener('submit', function (event) {
-
         event.preventDefault(); // prevents the form from contacting our server automatically (we want to do it ourselves)
         var formActionUrl = form.action; // 'form.action' is the url '/create-post'
         var formData = new FormData(form);
-
         postBlogposts(formActionUrl, formData);
     });
+
 }
 
 /****
  * Function definitions
  ***/
-function postBlogposts (url, data) {
-    fetch(url, {
-        method: 'POST',
-        body: data
+function postBlogposts(url, data) {
+  fetch(url, {
+    method: "POST",
+    body: data
+  })
+    .then(function(res) {
+      res.json().then(function(json) {
+        addBlogpostsToPage(json);
+        document.querySelector("form").reset();
+        window.location.href = "http://localhost:3000/"
+      });
     })
-    .then(function (res) {
-        res.json()
-            .then(function (json) {
-                console.log(json);
-                addBlogpostsToPage(json);
-                document.querySelector('form').reset();
-        })
-    })
-    .catch(function (err) {
-        console.error(err)
+    .catch(function(err) {
+      console.error(err);
     });
 }
 
-function getBlogposts (url) {
+function getBlogposts(url) {
     fetch(url, {
         method: 'GET'
     })
-    .then(function (res) {
-        res.json()
-        .then(function (json) {
-            console.log(json);
-            addBlogpostsToPage(json);
+        .then(function (res) {
+            res.json()
+                .then(function (json) {
+                    addBlogpostsToPage(json);
+                });
+        })
+        .catch(function (err) {
+            console.error(err)
         });
-    })
-    .catch(function (err) {
-        console.error(err)
-    });
 }
 
 function addBlogpostsToPage (data) {
-    for (var blogpost in data) {
-        if (data.hasOwnProperty(blogpost)) {
-
-            var postDiv         = document.createElement('div');
-            var postText        = document.createElement('p');
-            var thumbnail       = document.createElement('img');
-            var postContainer   = document.querySelector('.post-container');
+    JSON.parse(data).map(post => {  
+          if (post.hasOwnProperty("blogpost")) {
+            var postDiv = document.createElement("div");
+            var postText = document.createElement("p");
+            var thumbnail = document.createElement("img");
+            var postContainer = document.querySelector(".post-container");
 
             thumbnail.src = "./img/logo2.png";
             thumbnail.className = "thumbnail";
-            postText.innerHTML = data[blogpost];
+            postText.innerHTML = post.blogpost;
             postDiv.className = "post";
 
             postDiv.appendChild(thumbnail);
             postDiv.appendChild(postText);
             postContainer.appendChild(postDiv);
-        }
-    }
+          }
+    })   
 }
---
+++ server.js
@@ -0,0 +1,37 @@
+//core modules
+const fs = require('fs')
+
+//3rd party modules
+const express = require('express')
+const formidable = require("express-formidable");
+const hbs = require('hbs')
+//variables
+const port = 3000
+
+//create app
+const app = express()
+
+//serving public assets middleware
+app.use(express.static('public', {'extensions': ['html']}))
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+hbs.registerHelper("getCurrentYear", () => new Date().getFullYear());
+hbs.registerHelper("setUpper", text => text.toUpperCase());
+app.use(formidable());
+
+// router
+app.get('/posts', (req, res) => {
+    res.json(fs.readFileSync(__dirname + "/data/posts.json", "utf8"));   
+})
+app.post('/post', (req, res) => {
+    const pathPostsFile = __dirname + "/data/posts.json";
+    const allposts = JSON.parse(fs.readFileSync(pathPostsFile).toString());
+
+    const post = req.fields;
+    allposts.push(post)
+    fs.writeFileSync(pathPostsFile, JSON.stringify(allposts));
+    res.json(fs.readFileSync(pathPostsFile, "utf8"));
+})
+app.listen(port, (req, res) => {
+    console.log("Server is runnig on port:", port); 
+})
